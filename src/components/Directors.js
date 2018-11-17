13a20d10c596d429458d6291e13961129ac6991e
Katrina Pritchard <pritckat@gmail.com>
2018-11-17 03:03:58 +0000

    automatically backed up by learn

--- src/components/Directors.js
+++ src/components/Directors.js
@@ -5,6 +5,10 @@ const Directors = () => {
   return (
     <div>
       <h1>Directors Page</h1>
+      {directors.map((director, index) =>
+        <div key={index}>
+          <h2>{director.name}</h2>
+        </div>)}
     </div>
   );
 }
