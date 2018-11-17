e589d999c7a6e9d35e35d6a47fff6919260cc16c
Katrina Pritchard <pritckat@gmail.com>
2018-11-17 03:09:35 +0000

    automatically backed up by learn

--- src/components/Movies.js
+++ src/components/Movies.js
@@ -5,6 +5,9 @@ const Movies = () => {
   return (
     <div>
         <h1>Movies Page</h1>
+        {movies.map((movie, index) => (
+          <div key={index}>{movie}</div>
+        ))}
     </div>
   );
 };
