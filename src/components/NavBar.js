2de9024eccac31144e9ba4fd6a15c14b6950624f
Katrina Pritchard <pritckat@gmail.com>
2018-11-17 03:14:49 +0000

    automatically backed up by learn

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -4,7 +4,10 @@ import { NavLink } from 'react-router-dom';
 const NavBar = () => {
   return (
     <div className="navbar">
-      {/*{code here}*/}
+      <NavLink to="/">Home</NavLink>
+      <NavLink to="/actors">Actors</NavLink>
+      <NavLink to="/directors">Directors</NavLink>
+      <NavLink to="/movies">Movies</NavLink>
     </div>
   );
 };
